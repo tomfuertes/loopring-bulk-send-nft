8c454a8f1dea639c32acd6e697c45086139f9e82
Tom Fuertes <tomfuertes@gmail.com>
2022-05-21 16:29:32 -0500

    can't figure out sign

---
+++ src/loopring.ts
@@ -0,0 +1,35 @@
+import * as sdk from "@loopring-web/loopring-sdk";
+
+// console.log(sdk);
+
+export default class LoopringAPIClass {
+  public static userAPI: sdk.UserAPI;
+  public static exchangeAPI: sdk.ExchangeAPI;
+  public static ammpoolAPI: sdk.AmmpoolAPI;
+  public static walletAPI: sdk.WalletAPI;
+  public static wsAPI: sdk.WsAPI;
+  public static nftAPI: sdk.NFTAPI;
+  public static delegate: sdk.DelegateAPI;
+  public static globalAPI: sdk.GlobalAPI;
+  // public static contractAPI: sdk.ContractAPI;
+  public static __chainId__: sdk.ChainId;
+  public static InitApi = (chainId: sdk.ChainId) => {
+    LoopringAPIClass.userAPI = new sdk.UserAPI({ chainId });
+    LoopringAPIClass.exchangeAPI = new sdk.ExchangeAPI({ chainId });
+    LoopringAPIClass.globalAPI = new sdk.GlobalAPI({ chainId });
+    LoopringAPIClass.ammpoolAPI = new sdk.AmmpoolAPI({ chainId });
+    LoopringAPIClass.walletAPI = new sdk.WalletAPI({ chainId });
+    LoopringAPIClass.wsAPI = new sdk.WsAPI({ chainId });
+    LoopringAPIClass.nftAPI = new sdk.NFTAPI({ chainId });
+    LoopringAPIClass.delegate = new sdk.DelegateAPI({ chainId });
+    LoopringAPIClass.__chainId__ = chainId;
+    // LoopringAPI.contractAPI = ContractAPI;
+  };
+}
+
+/* env:
+ * test:  sdk.ChainId.GOERLI
+ * eth:  sdk.ChainId.MAINNET
+ */
+
+LoopringAPIClass.InitApi(sdk.ChainId.MAINNET);
--- src/useConnect.ts
+++ src/useConnect.ts
@@ -1,3 +1,5 @@
+import * as sdk from "@loopring-web/loopring-sdk";
+
 import { ChainId, useConnectHook } from "./useConnectHook";
 
 export function useConnect() {
@@ -15,18 +17,38 @@ export function useConnect() {
     },
     handleConnect: async ({
       accounts,
-      chainId
+      provider,
+      chainId,
     }: {
       accounts: string;
       provider: any;
       chainId: ChainId | "unknown";
     }) => {
       const accAddress = accounts[0];
+      // console.log(provider, accounts, accAddress, chainId);
+
+      // console.log(loopring);
+
       console.log(
         "After connect >>,network part start: step1 networkUpdate",
         accAddress
       );
+
+      const opts = {
+        web3: provider,
+        address: accAddress,
+        keySeed: sdk.GlobalAPI.KEY_MESSAGE.replace(
+          "${exchangeAddress}",
+          "0x0BABA1Ad5bE3a5C0a66E7ac838a129Bf948f1eA4"
+        ).replace("${nonce}", (0).toString()),
+        walletType: sdk.ConnectorNames.MetaMask,
+        chainId: sdk.ChainId.MAINNET,
+      };
+      console.log(opts);
+      const eddsaKey = await sdk.generateKeyPair(opts);
+
+      console.log(eddsaKey);
       console.log("After connect >>,network part done: step2 check account");
-    }
+    },
   });
 }
